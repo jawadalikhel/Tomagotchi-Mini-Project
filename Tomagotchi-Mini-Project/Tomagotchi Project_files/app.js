109688bc375689d2745abc05914648877edc3ae3
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-26 08:23:23 -0500

    finished the html page

---
+++ Tomagotchi-Mini-Project/Tomagotchi Project_files/app.js
@@ -0,0 +1,12 @@
+$('.feed').on('click', ()=>{
+	let $hunger = $('.Hunger').val();
+	$hunger += $hunger;
+	// $hunger = Number($hunger);
+	console.log(typeof $hunger, $hunger)
+
+});
+
+
+// const feedPet = () =>{
+	
+// }
---
+++ Tomagotchi-Mini-Project/app.js
@@ -0,0 +1,17 @@
+// $('.feed').on('click', ()=>{
+// 	let $hunger = $('.Hunger').val();
+// 	$hunger += 1;
+// 	// $hunger = Number($hunger);
+// 	$('.Hunger').val($hunger)
+// 	console.log($hunger);
+
+// });
+
+// #('.Hunger').progressbar({
+// 	value: 0
+// })
+
+
+// // const feedPet = () =>{
+	
+// // }
