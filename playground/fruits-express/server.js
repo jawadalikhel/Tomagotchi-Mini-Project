fb668b674a7b591f63fb788b0f23eabd0776d820
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-11-07 10:05:08 -0600

    first commit

---
+++ playground/fruits-express/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app     = express();
+const Fruits = require('./models/fruits.js')
+
+
+
+
+
+
+
+app.listen(3000, () =>{
+  console.log('hola i am alive now bitch');
+})
